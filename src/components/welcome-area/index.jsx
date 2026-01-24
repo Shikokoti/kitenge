105aef4d80bade2c5715cb1cf1a64d724331b91a
Ellis Lunayo <ellislunayo@gmail.com>
2026-01-24 11:51:13 +0300

    Copy changes on the rsvp section

--- src/components/welcome-area/index.jsx
+++ src/components/welcome-area/index.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import './style.css'
-import LocationMap from '../LocationModal/LocationModal'
+import React from 'react';
+import './style.css';
+import LocationMap from '../LocationModal/LocationModal';
 
-const Welcome = () =>{
-    return(
+const Welcome = () => {
+    return (
         <div className="welcome-area">
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
                         <div className="welcome-content">
                             <h2>Welcome to our big day</h2>
-                            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or less normal distribution of letters</p>
-                            <div className="btn"><Link to="/home">RSVP</Link></div>
+                            <p>
+                                Join us as we celebrate love, commitment, and the start
+                                of a beautiful journey together.
+                            </p>
+
+                            {/* Scroll to RSVP Section */}
+                            <div className="btn">
+                                <a href="#rsvp">RSVP</a>
+                            </div>
+
                             <LocationMap buttonClass={'location-btn'} />
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-    )
-}
+    );
+};
 
 export default Welcome;
