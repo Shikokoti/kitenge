337bd64b786a1dd06be637293c1c4936d5205919
Ellis Lunayo <ellislunayo@gmail.com>
2026-01-24 12:35:56 +0300

    updated the images of the bride and groom men

--- src/components/people/index.jsx
+++ src/components/people/index.jsx
@@ -2,26 +2,50 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Sectiontitle from "../section-title";
 
-import bride1 from "../../images/groomsmen-bridesmaid/groom.png";
-import bride2 from "../../images/groomsmen-bridesmaid/groom.png";
-import bride3 from "../../images/groomsmen-bridesmaid/groom.png";
-import bride4 from "../../images/groomsmen-bridesmaid/groom.png";
+import groom1 from "../../images/groomsmen-bridesmaid/groom.png";
+import groom2 from "../../images/groomsmen-bridesmaid/groom.png";
+import groom3 from "../../images/groomsmen-bridesmaid/groom.png";
+import groom4 from "../../images/groomsmen-bridesmaid/groom.png";
+import groom5 from "../../images/groomsmen-bridesmaid/groom.png";
+import groom6 from "../../images/groomsmen-bridesmaid/groom.png";
+
+import bride1 from "../../images/groomsmen-bridesmaid/bride.png";
+import bride2 from "../../images/groomsmen-bridesmaid/bride.png";
+import bride3 from "../../images/groomsmen-bridesmaid/bride.png";
+import bride4 from "../../images/groomsmen-bridesmaid/bride.png";
 import bride5 from "../../images/groomsmen-bridesmaid/bride.png";
 import bride6 from "../../images/groomsmen-bridesmaid/bride.png";
-import bride7 from "../../images/groomsmen-bridesmaid/bride.png";
-import bride8 from "../../images/groomsmen-bridesmaid/bride.png";
-import bride9 from "../../images/groomsmen-bridesmaid/bride.png";
-import bride10 from "../../images/groomsmen-bridesmaid/bride.png";
 
 import "./style.css";
 
 const People = () => {
   const [activeTab, setActiveTab] = useState("groom");
 
+  // Groomsmen Data
+  const groomsmen = [
+    { name: "Brian Lunayo", role: "GROOMSMAN", img: groom1 },
+    { name: "Abdi Aziz Noor", role: "GROOMSMAN", img: groom2 },
+    { name: "Rashid Mnakara", role: "GROOMSMAN", img: groom3 },
+    { name: "Mitch Okello", role: "GROOMSMAN", img: groom4 },
+    { name: "Stephen Muthama", role: "GROOMSMAN", img: groom5 },
+    { name: "Erick Haul", role: "GROOMSMAN", img: groom6 },
+    { name: "Diego Harkamisco", role: "GROOMSMAN", img: groom6 },
+  ];
+
+  // Bridesmaids Data
+  const bridesmaids = [
+    { name: "Lily Brown", role: "Sister", img: bride1 },
+    { name: "Anna White", role: "Best Friend", img: bride2 },
+    { name: "Sophia Green", role: "Cousin", img: bride3 },
+    { name: "Emily Black", role: "Friend", img: bride4 },
+    { name: "Grace Adams", role: "Friend", img: bride5 },
+    { name: "Olivia King", role: "Friend", img: bride6 },
+  ];
+
   return (
     <div id="people" className="person-area section-padding pb-70">
       <div className="container">
-        <Sectiontitle section="Groomsmen & Bridesmaid" />
+        <Sectiontitle section="Groomsmen & Bridesmaids" />
 
         <div className="person-area-menu">
           {/* TABS */}
@@ -31,7 +55,7 @@ const People = () => {
                 className={`nav-link ${activeTab === "groom" ? "active" : ""}`}
                 onClick={() => setActiveTab("groom")}
               >
-                Groomsman
+                Groomsmen
               </button>
             </li>
             <li className="nav-item">
@@ -39,7 +63,7 @@ const People = () => {
                 className={`nav-link ${activeTab === "bride" ? "active" : ""}`}
                 onClick={() => setActiveTab("bride")}
               >
-                Bridemaids
+                Bridesmaids
               </button>
             </li>
           </ul>
@@ -50,31 +74,38 @@ const People = () => {
             {activeTab === "groom" && (
               <div className="tab-pane fade show active">
                 <div className="row">
-                  {[bride1, bride2, bride3, bride4, bride5, bride6].map(
-                    (img, i) => (
-                      <div
-                        className="col-lg-4 col-md-6 col-sm-6"
-                        key={i}
-                      >
-                        <div className="person-wrap">
-                          <div className="person-img">
-                            <img src={img} alt="" />
-                            <div className="social-list">
-                              <ul>
-                                <li><Link to="/"><i className="fa fa-facebook-f" /></Link></li>
-                                <li><Link to="/"><i className="fa fa-twitter" /></Link></li>
-                                <li><Link to="/"><i className="fa fa-skype" /></Link></li>
-                              </ul>
-                            </div>
-                          </div>
-                          <div className="person-content">
-                            <h3>Brain Lunayo</h3>
-                            <span>Brother</span>
+                  {groomsmen.map((person, i) => (
+                    <div className="col-lg-4 col-md-6 col-sm-6" key={i}>
+                      <div className="person-wrap">
+                        <div className="person-img">
+                          <img src={person.img} alt={person.name} />
+                          <div className="social-list">
+                            <ul>
+                              <li>
+                                <Link to="/">
+                                  <i className="fa fa-facebook-f" />
+                                </Link>
+                              </li>
+                              <li>
+                                <Link to="/">
+                                  <i className="fa fa-twitter" />
+                                </Link>
+                              </li>
+                              <li>
+                                <Link to="/">
+                                  <i className="fa fa-skype" />
+                                </Link>
+                              </li>
+                            </ul>
                           </div>
                         </div>
+                        <div className="person-content">
+                          <h3>{person.name}</h3>
+                          <span>{person.role}</span>
+                        </div>
                       </div>
-                    )
-                  )}
+                    </div>
+                  ))}
                 </div>
               </div>
             )}
@@ -83,28 +114,38 @@ const People = () => {
             {activeTab === "bride" && (
               <div className="tab-pane fade show active">
                 <div className="row">
-                  {[bride5, bride6, bride7, bride8, bride9, bride10].map(
-                    (img, i) => (
-                      <div className="col-lg-4 col-md-6" key={i}>
-                        <div className="person-wrap">
-                          <div className="person-img">
-                            <img src={img} alt="" />
-                            <div className="social-list">
-                              <ul>
-                                <li><Link to="/"><i className="fa fa-facebook-f" /></Link></li>
-                                <li><Link to="/"><i className="fa fa-twitter" /></Link></li>
-                                <li><Link to="/"><i className="fa fa-skype" /></Link></li>
-                              </ul>
-                            </div>
-                          </div>
-                          <div className="person-content">
-                            <h3>Lily Brown</h3>
-                            <span>Sister</span>
+                  {bridesmaids.map((person, i) => (
+                    <div className="col-lg-4 col-md-6 col-sm-6" key={i}>
+                      <div className="person-wrap">
+                        <div className="person-img">
+                          <img src={person.img} alt={person.name} />
+                          <div className="social-list">
+                            <ul>
+                              <li>
+                                <Link to="/">
+                                  <i className="fa fa-facebook-f" />
+                                </Link>
+                              </li>
+                              <li>
+                                <Link to="/">
+                                  <i className="fa fa-twitter" />
+                                </Link>
+                              </li>
+                              <li>
+                                <Link to="/">
+                                  <i className="fa fa-skype" />
+                                </Link>
+                              </li>
+                            </ul>
                           </div>
                         </div>
+                        <div className="person-content">
+                          <h3>{person.name}</h3>
+                          <span>{person.role}</span>
+                        </div>
                       </div>
-                    )
-                  )}
+                    </div>
+                  ))}
                 </div>
               </div>
             )}
