f165297a1cc9638b23823d2de5d9ef9e61971596
Ellis Lunayo <ellislunayo@gmail.com>
2026-01-27 22:45:31 +0300

    changed the reception  time

--- src/components/location/index.jsx
+++ src/components/location/index.jsx
@@ -25,7 +25,7 @@ const Location = () => {
                             <div className="col-lg-7">
                                 <div className="ceromony-content">
                                     <h3>Wedding Church Ceremony</h3>
-                                    <span>Saturday, 21 March 26, 10.00AM-11.00 AM</span>
+                                    <span>Saturday, 21 March 26, 10.00 AM-11.00 AM</span>
                                     <span>Nairobi Kenya, Brookhaven Grounds</span>
                                     <p>Come and celebrate with us!</p>
                                     <LocationMap buttonClass={'location-btn'} />
@@ -39,7 +39,7 @@ const Location = () => {
                         <div className="col-lg-7">
                             <div className="ceromony-content ceromony-content2">
                                 <h3>Reception</h3>
-                                <span>Saturday, 21 March 26, 12.30 AM-4.00 PM</span>
+                                <span>Saturday, 21 March 26, 12.30 PM-4.00 PM</span>
                                 <span>Nairobi Kenya, Brookhaven Grounds</span>
                                 <p>Come and celebrate with us!</p>
                                 <LocationMap buttonClass={'location-btn'} />
@@ -61,8 +61,8 @@ const Location = () => {
                         </div>
                         <div className="col-lg-7">
                             <div className="ceromony-content">
-                                <h3>Family & Friends Speeches</h3>
-                                <span>Saturday, 21 March 26, 3.00 AM-5.30 PM</span>
+                                <h3>Cake Cutting</h3>
+                                <span>Saturday, 21 March 26, 4.30 PM-5.30 PM</span>
                                 <span>Nairobi Kenya, Brookhaven Grounds</span>
                                 <p>Come and celebrate with us!</p>
                                 <LocationMap buttonClass={'location-btn'} />
